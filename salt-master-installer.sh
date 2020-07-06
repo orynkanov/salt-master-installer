3c67ed0a34b8e8ed4ef77522f3ee20184cd13486
orynkanov <orynkanov@gmail.com>
2020-07-06 23:05:49 +0600

    fix and autostart salt

--- salt-master-installer.sh
+++ salt-master-installer.sh
@@ -8,9 +8,13 @@ OSVER=$(grep -o '[0-9]' /etc/centos-release | head -n1)
 if [[ $OSVER -eq 7 ]]; then
     yum install https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm
     yum clean all
-    yum install -y salt-master salt-minion salt-ssh
+    yum install -y salt-master salt-api salt-ssh
 elif [[ $OSVER -eq 8 ]]; then
     dnf install -y https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el8.noarch.rpm
     dnf clean all
-    dnf install -y salt-master salt-minion salt-ssh
+    dnf install -y salt-master salt-api salt-ssh
 fi
+
+systemctl enable salt-master
+systemctl enable salt-api
+firewall-cmd --permanent --add-service=salt-master
