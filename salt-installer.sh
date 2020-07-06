dc138ff5ac22f1bbd832616e2a9481fe38640ca1
orynkanov <orynkanov@gmail.com>
2020-07-06 22:30:35 +0600

    first commit

---
+++ salt-installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! -f /etc/centos-release ]]; then
+    echo OS not CentOS. This script only for CentOS!
+    exit 1
+fi
+OSVER=$(grep -o '[0-9]' /etc/centos-release | head -n1)
+if [[ $OSVER -eq 7 ]]; then
+    yum install https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm
+    yum clean all
+    yum install -y salt-master salt-minion salt-ssh
+elif [[ $OSVER -eq 8 ]]; then
+    dnf install -y https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el8.noarch.rpm
+    dnf clean all
+    dnf install -y salt-master salt-minion salt-ssh
+fi
