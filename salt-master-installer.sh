419d4e5b194904060b5c46949fbb4294aa453e49
orynkanov <orynkanov@gmail.com>
2020-07-07 18:19:28 +0600

    test gitfs

--- salt-master-installer.sh
+++ salt-master-installer.sh
@@ -2,10 +2,13 @@
 
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+#start check
 if [[ ! -f /etc/centos-release ]]; then
     echo OS not CentOS. This script only for CentOS!
     exit 1
 fi
+#finish check
+
 OSVER=$(grep -o '[0-9]' /etc/centos-release | head -n1)
 if [[ $OSVER -eq 7 ]]; then
     yum install -y https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el7.noarch.rpm
@@ -14,10 +17,10 @@ if [[ $OSVER -eq 7 ]]; then
 elif [[ $OSVER -eq 8 ]]; then
     dnf install -y https://repo.saltstack.com/py3/redhat/salt-py3-repo-latest.el8.noarch.rpm
     dnf clean all
-    dnf install -y salt-master salt-api salt-ssh
+    dnf install -y salt-master salt-api salt-ssh python3-pygit2
 fi
 
-cp "$SCRIPTDIR"/master.d/master.conf /etc/salt/master.d/master.conf
+cp "$SCRIPTDIR"/master.d/* /etc/salt/master.d/
 
 systemctl enable salt-master --now
 systemctl enable salt-api --now
